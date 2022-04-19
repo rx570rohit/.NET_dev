ab49cdbee167dc81a87d333ea61b271c7e3fee4e
rx570rohit <101522761+rx570rohit@users.noreply.github.com>
2022-04-19 18:46:14 +0530

    Add .gitattributes and .gitignore.

---
+++ emp_wage/empmain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp_wage
+{
+    internal class empmain
+    {
+        public static void Main(String [ ]arg)
+        {
+            empcal.empcalculation();
+        }
+    }
+}
